5cefdca8f317b48a9f26e8d1e2ee959a409412ed
canhchimlac94 <tranvandung.humg@gmail.com>
2023-05-05 07:52:33 +0000

    update service

--- src/service/solicalService.js
+++ src/service/solicalService.js
@@ -1,9 +1,22 @@
 const axios = require("axios");
+const headers = {
+  'accept': 'application/json, text/javascript, */*; q=0.01',
+  'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+  'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
+  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+  // 'cookie': 'current_language=en; _ga=GA1.1.115940210.1660795490; _gcl_au=1.1.669324151.1660795490; _ga_5370HT04Z3=GS1.1.1660795489.1.1.1660795513.0.0.0',
+};
 
 const ssyoutube = async (url) => {
   return axios.post("https://ssyoutube.com/api/convert", {
     url: url,
-  }).then(res => res.data);
+  }, {
+    headers
+  })
+    .then(res => res.data)
+    .catch(err => {
+      return {}
+    });
 };
 
 module.exports = {
